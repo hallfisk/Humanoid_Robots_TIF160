1d02937236930b835eddfb72afd5e85df857d2f7
Johan Larsson <75022240+hallfisk@users.noreply.github.com>
2024-10-07 15:58:07 +0200

    Add files via upload

--- Hubert_DemoApp_ver1.ino
+++ Hubert_DemoApp_ver1.ino
@@ -1,69 +1,50 @@
-/*
-  Hubert robot test program.
-
-  This sketch control Hubert's servos.  
-  A write.microseconds value in approx. [500,2350], mid pos.: 1350
-  NOTE: Go back to NeutralPos. before terminating.
-  
-  created 20 Jul 2020
-  by Krister Wolff
-  modified 20 Jul 2020
-  by Krister Wolff
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/
-*/
-
 #include <Arduino.h>
 #include <Servo.h>
 
-//Servos
+// Servos
 Servo body;
-Servo headPan;
-Servo headTilt;
 Servo shoulder;
 Servo elbow;
 Servo gripper;
 
-//Init position of all servos
-const int servo_pins[] = {3, 5, 6, 9, 10, 11};
-
-const int pos_init[] = {1700, 1500, 2000, 2200, 1650, 1600};
-int curr_pos[6];
-int new_servo_val[6];
+// Servo pins
+const int servo_pins[] = {3, 9, 10, 11}; // Pins for body, shoulder, elbow, and gripper
 
 const int pos_min[] = {560, 550, 950, 750, 550, 550};
-const int pos_max[] = {2330, 2340, 2400, 2200, 2400, 2150};
+const int pos_max[] = {2330, 2500, 2400, 2150};
 
-const int pos_move[] = {2200, 1500, 2000, 1100, 2300, 1600};
+// Init position of all servos
+const int pos_init[] = {1430, 1100, 1650, 1000}; // Initial positions for body, shoulder, elbow, and gripper
+int curr_pos[4]; // Stores current positions for body, shoulder, elbow, and gripper
+int new_servo_val[4];
 
-//Servo update function
-void servo_body_ex(const int new_pos) {
-
+// Function to move the body servo incrementally
+void servo_body(int new_pos) {
   int diff, steps, now, CurrPwm, NewPwm, delta = 6;
 
-  //current servo value
+  // Current servo value
   now = curr_pos[0];
   CurrPwm = now;
   NewPwm = new_pos;
 
+  // Determine direction of movement
   /* determine interation "diff" from old to new position */
   diff = (NewPwm - CurrPwm)/abs(NewPwm - CurrPwm); // Should return +1 if NewPwm is bigger than CurrPwm, -1 otherwise.
   steps = abs(NewPwm - CurrPwm);
   delay(10);
 
   for (int i = 0; i < steps; i += delta) {
-    now = now + delta*diff;
-    body.writeMicroseconds(now);
-    delay(20);
-  }
-  curr_pos[0] = now;
-  delay(10);
+      now = now + delta*diff;
+      body.writeMicroseconds(now);
+      delay(20);
+    }
+    curr_pos[0] = now;
+    delay(10);
+
 }
 
-//Servo update function
-void servo_neck_pan(const int new_pos) {
+
+void servo_shoulder(const int new_pos) {
 
   int diff, steps, now, CurrPwm, NewPwm, delta = 6;
 
@@ -79,7 +60,7 @@ void servo_neck_pan(const int new_pos) {
 
   for (int i = 0; i < steps; i += delta) {
     now = now + delta*diff;
-    headPan.writeMicroseconds(now);
+    shoulder.writeMicroseconds(now);
     delay(20);
   }
   curr_pos[1] = now;
@@ -87,7 +68,7 @@ void servo_neck_pan(const int new_pos) {
 }
 
 //Servo update function
-void servo_neck_tilt(const int new_pos) {
+void servo_elbow(const int new_pos) {
 
   int diff, steps, now, CurrPwm, NewPwm, delta = 6;
 
@@ -103,15 +84,14 @@ void servo_neck_tilt(const int new_pos) {
 
   for (int i = 0; i < steps; i += delta) {
     now = now + delta*diff;
-    headTilt.writeMicroseconds(now);
+    elbow.writeMicroseconds(now);
     delay(20);
   }
   curr_pos[2] = now;
   delay(10);
 }
 
-//Servo update function
-void servo_shoulder(const int new_pos) {
+void servo_gripper_ex(const int new_pos) {
 
   int diff, steps, now, CurrPwm, NewPwm, delta = 6;
 
@@ -127,85 +107,25 @@ void servo_shoulder(const int new_pos) {
 
   for (int i = 0; i < steps; i += delta) {
     now = now + delta*diff;
-    shoulder.writeMicroseconds(now);
+    gripper.writeMicroseconds(now);
     delay(20);
   }
   curr_pos[3] = now;
   delay(10);
 }
 
-//Servo update function
-void servo_elbow(const int new_pos) {
 
-  int diff, steps, now, CurrPwm, NewPwm, delta = 6;
-
-  //current servo value
-  now = curr_pos[4];
-  CurrPwm = now;
-  NewPwm = new_pos;
-
-  /* determine interation "diff" from old to new position */
-  diff = (NewPwm - CurrPwm)/abs(NewPwm - CurrPwm); // Should return +1 if NewPwm is bigger than CurrPwm, -1 otherwise.
-  steps = abs(NewPwm - CurrPwm);
-  delay(10);
-
-  for (int i = 0; i < steps; i += delta) {
-    now = now + delta*diff;
-    elbow.writeMicroseconds(now);
-    delay(20);
-  }
-  curr_pos[4] = now;
-  delay(10);
-}
-
-//Servo update function
-void servo_gripper_ex(const int new_pos) {
-
-  int diff, steps, now, CurrPwm, NewPwm, delta = 6;
-
-  //current servo value
-  now = curr_pos[5];
-  CurrPwm = now;
-  NewPwm = new_pos;
-
-  /* determine interation "diff" from old to new position */
-  diff = (NewPwm - CurrPwm)/abs(NewPwm - CurrPwm); // Should return +1 if NewPwm is bigger than CurrPwm, -1 otherwise.
-  steps = abs(NewPwm - CurrPwm);
-  delay(10);
-
-  for (int i = 0; i < steps; i += delta) {
-    now = now + delta*diff;
-    gripper.writeMicroseconds(now);
-    delay(20);
-  }
-  curr_pos[5] = now;
-  delay(10);
-}
 
 void setup() {
-
-  Serial.begin(57600); // Starts the serial communication
-
-
-	//Attach each joint servo
-	//and write each init position
+  // Attach each servo to its corresponding pin
   body.attach(servo_pins[0]);
   body.writeMicroseconds(pos_init[0]);
-  
-  headPan.attach(servo_pins[1]);
-  headPan.writeMicroseconds(pos_init[1]);
-  
-  headTilt.attach(servo_pins[2]);
-  headTilt.writeMicroseconds(pos_init[2]);
-
-  shoulder.attach(servo_pins[3]);
-	shoulder.writeMicroseconds(pos_init[3]);
-
-	elbow.attach(servo_pins[4]);
-	elbow.writeMicroseconds(pos_init[4]);
-	
-	gripper.attach(servo_pins[5]);
-  gripper.writeMicroseconds(pos_init[5]);
+  shoulder.attach(servo_pins[1]);
+  shoulder.writeMicroseconds(pos_init[1]);
+  elbow.attach(servo_pins[2]);
+  elbow.writeMicroseconds(pos_init[2]);
+  gripper.attach(servo_pins[3]);
+  gripper.writeMicroseconds(pos_init[3]);
 
   //Initilize curr_pos and new_servo_val vectors
   byte i;
@@ -214,28 +134,111 @@ void setup() {
     new_servo_val[i] = curr_pos[i];
   }
 
-	delay(2000);
+  Serial.begin(57600);
+  
+  /*
+  if (Serial.available() > 0) {
+    String data = Serial.readStringUntil('\n');
+    data.trim(); // Remove any whitespace or newline characters
+    Serial.println("Received data: " + data);
+
+    // Split the data by commas
+    int index1 = data.indexOf(',');
+    int index2 = data.indexOf(',', index1 + 1);
+
+    if (index1 == -1 || index2 == -1) {
+      Serial.println("Error: Received data in wrong format.");
+      return;
+    }
+
+    String body_str = data.substring(0, index1);
+    String shoulder_str = data.substring(index1 + 1, index2);
+    String elbow_str = data.substring(index2 + 1);
+
+    float desired_body_pos = body_str.toFloat();
+    float desired_shoulder_pos = shoulder_str.toFloat();
+    float desired_elbow_pos = elbow_str.toFloat();
+
+    Serial.print("Body: ");
+    Serial.println(desired_body_pos);
+    Serial.print("Shoulder: ");
+    Serial.println(desired_shoulder_pos);
+    Serial.print("Elbow: ");
+    Serial.println(desired_elbow_pos);
+
+    int desired_gripper_pos = 1400; // Example: desired gripper positiom
+
+    // Move arm above box
+    servo_elbow(600);
+    delay(500);
+    servo_shoulder(2100);
+    delay(500);
+
+    // Move to fruit pos
+    servo_body(desired_body_pos);
+    delay(500);
+    servo_elbow(desired_elbow_pos);
+    delay(500);
+    servo_shoulder(desired_shoulder_pos);
+    delay(500);
+    servo_gripper_ex(desired_gripper_pos);
+    delay(500);
+  }
+  */
+  
 }
 
-void loop() {
 
-  delay(1000);
-  servo_body_ex(pos_move[0]);
-  delay(1000);
-  servo_shoulder(pos_move[3]);
-  delay(1000);
-  
-  byte j;
-  for(j=0; j<5; j++){
-    servo_elbow(pos_move[4]);
-    delay(100);
-    servo_elbow(pos_init[4]);
-    delay(100);
+
+void loop() {
+  //Serial.print("Arduino is ready.");
+  int a = 0;
+  if (Serial.available() > 0 && a == 0) {
+    a = 1;
+    String data = Serial.readStringUntil('\n');
+    data.trim(); // Remove any whitespace or newline characters
+    Serial.println("Received data: " + data);
+
+    // Split the data by commas
+    int index1 = data.indexOf(',');
+    int index2 = data.indexOf(',', index1 + 1);
+
+    if (index1 == -1 || index2 == -1) {
+      Serial.println("Error: Received data in wrong format.");
+      return;
+    }
+
+    String body_str = data.substring(0, index1);
+    String shoulder_str = data.substring(index1 + 1, index2);
+    String elbow_str = data.substring(index2 + 1);
+
+    float desired_body_pos = body_str.toFloat();
+    float desired_shoulder_pos = shoulder_str.toFloat();
+    float desired_elbow_pos = elbow_str.toFloat();
+
+    Serial.print("Body: ");
+    Serial.println(desired_body_pos);
+    Serial.print("Shoulder: ");
+    Serial.println(desired_shoulder_pos);
+    Serial.print("Elbow: ");
+    Serial.println(desired_elbow_pos);
+
+    int desired_gripper_pos = 1400; // Example: desired gripper positiom
+
+    // Move arm above box
+    servo_elbow(600);
+    delay(500);
+    servo_shoulder(2100);
+    delay(500);
+
+    // Move to fruit pos
+    servo_body(desired_body_pos);
+    delay(500);
+    servo_elbow(desired_elbow_pos);
+    delay(500);
+    servo_shoulder(desired_shoulder_pos);
+    delay(500);
+    servo_gripper_ex(desired_gripper_pos);
+    delay(500);
   }
-  
-  delay(1000);
-  servo_shoulder(pos_init[3]);
-	delay(1000);
-  servo_body_ex(pos_init[0]);
-  delay(2000);
 }
